821b8683fb0b5dab8ae500eb28ac0cce7b3f9187
Jaden Wang <jadenyjw@gmail.com>
2017-02-21 17:02:03 -0500

    Finish Phrase Finder

---
+++ Files/Phrase.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Phrase{
+  public static void main(String[] args){
+
+    //Ask for path and phrase information
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Please enter the absolute path of the folder you wish to list.");
+    String path = input.nextLine();
+
+    System.out.println("Please enter a phrase you wish to search for.");
+    String phrase = input.nextLine();
+
+    System.out.println("What would you like to replace this phrase with?");
+    String replacement = input.nextLine();
+
+    //Initialize variables outside of the try block
+    File folder = null;
+    File[] files;
+    FileReader fr;
+    FileWriter fw;
+    BufferedReader reader;
+    BufferedWriter writer;
+    String name;
+
+    try{
+      folder = new File(path);
+      //Store all files into an array
+      files = folder.listFiles();
+      System.out.println("Replaced " + phrase + " with " + replacement + " in these files:");
+
+      for(int x = 0; x < files.length; x++){
+
+        fr = new FileReader(files[x]);
+        reader = new BufferedReader(fr);
+        //Initialize a writer in a temporary file.
+        fw = new FileWriter("tmp");
+        writer = new BufferedWriter(fw);
+
+        boolean isRead = false;
+        //Read through all the lines.
+        while ((name = reader.readLine()) != null) {
+          //Look for the line that they specified.
+          if(name.contains(phrase)){
+            if(isRead == false){
+              System.out.println(files[x].getName());
+              isRead = true;
+            }
+            name = name.replace(phrase, replacement);
+          }
+
+          writer.write(name + "\n");
+        }
+        //Close IO objects.
+        reader.close();
+        fr.close();
+        writer.close();
+        fw.close();
+
+        //Replace old file with temporary file.
+        File old = new File(files[x].getAbsolutePath());
+        old.delete();
+        File newFile = new File("tmp");
+        newFile.renameTo(old);
+
+
+      }
+
+
+    }
+    catch (Exception e){
+      System.out.println(e);
+    }
+
+
+  }
+}
--- Files/TestProcessor.java
+++ Files/TestProcessor.java
@@ -3,6 +3,7 @@ import java.io.*;
 public class TestProcessor{
 
   public static void main(String[] args){
+    //Initialize variables
     File myFile = new File("test.txt");
     FileReader fr;
     FileWriter fw;
@@ -11,6 +12,7 @@ public class TestProcessor{
     String name;
 
     try {
+      //Define variables inside the try block
       fr = new FileReader(myFile);
       fw = new FileWriter ("marks.dat");
       reader = new BufferedReader(fr);
@@ -22,29 +24,31 @@ public class TestProcessor{
 
         double score = 0.0;
         String answers = reader.readLine();
-
-
+        //Finds the number of correct answers.
         for(int x = 0; x < answers.length(); x++){
-        
+
           if (answers.charAt(x) == correct[x]){
             score++;
           }
 
         }
-        int percent = (int) ((score / correct.length) * 100);
+        int percent = (int) (score / correct.length * 100);
+        //Writes the correct answers.
         writer.write(name + " " + percent + "%\n");
 
       }
+
+      //Close defined objects
       reader.close();
       fr.close();
       writer.close();
       fw.close();
     }
     catch (FileNotFoundException e) {
-
+      System.out.println("File test.txt not found.")
     }
     catch (IOException e) {
-
+      System.out.println("Oops. Something went wrong...")
     }
 
   }
