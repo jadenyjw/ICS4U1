36bb1399cb83c8103ca05f69461ecbbee0779a6f
Jaden Wang <jadenyjw@gmail.com>
2017-04-13 13:41:38 -0400

    Work on layouts

--- WangJ_Layouts/Main.java
+++ WangJ_Layouts/Main.java
@@ -9,11 +9,11 @@ public class Main{
   public static void main(String[] args){
 
 
-    //Q2 q2 = new Q2();
+    Q2 q2 = new Q2();
 
-    //Q3 q3 = new Q3();
+    Q3 q3 = new Q3();
 
-    //Q4 q4 = new Q4();
+    Q4 q4 = new Q4();
 
     Q5 q5 = new Q5();
   }
