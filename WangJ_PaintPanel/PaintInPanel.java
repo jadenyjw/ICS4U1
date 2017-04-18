cc462aba338555454a1c0e118dbddc4d45d90de0
Jaden Wang <jadenyjw@gmail.com>
2017-04-18 13:32:31 -0400

    Finish painting

--- WangJ_PaintPanel/PaintInPanel.java
+++ WangJ_PaintPanel/PaintInPanel.java
@@ -1,5 +1,15 @@
+/*************************************************************
+Name: Jaden Wang
+Course: ICS4U1
+Date: April 18, 2017
+Teacher: Ms. Strelkovska
+Assignment: Paint in Panel
+*************************************************************/
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+
 
 public class PaintInPanel extends JFrame{
 
@@ -19,40 +29,65 @@ public class PaintInPanel extends JFrame{
 
 }
 
-class PaintPanel extends JPanel{
+class PaintPanel extends JPanel implements ActionListener{
 
+  CirclesPanel circles = new CirclesPanel();
+  ButtonPanel buttons = new ButtonPanel();
   public PaintPanel(){
     this.setLayout(new BorderLayout());
-    this.add(new CirclesPanel(), BorderLayout.CENTER);
-    this.add(new ButtonPanel(), BorderLayout.SOUTH);
+    this.add(circles, BorderLayout.CENTER);
+    buttons.draw.addActionListener(this);
+    buttons.exit.addActionListener(this);
+    this.add(buttons, BorderLayout.SOUTH);
   }
+
+  public void actionPerformed(ActionEvent e){
+    if(e.getActionCommand().equals("Draw")){
+      circles.drawing=true;
+      circles.repaint();
+    }
+    else{
+      System.exit(0);
+    }
+  }
+
 
 }
 
 class CirclesPanel extends JPanel{
+
+  public boolean drawing = false;
 
   public CirclesPanel(){
 
   }
 
   public void paintComponent(Graphics g){
-    int height = this.getHeight();
-    int width = this.getWidth();
+    super.paintComponent(g);
 
-    for(int x = 0; x < width/50; x++){
-      for(int y = 0; y < height/50; y++){
-        g.fillOval(x*50, y*50, 30, 30);
+    if(drawing){
+      int height = this.getHeight();
+      int width = this.getWidth();
+
+      for(int x = 0; x < width/50; x++){
+        for(int y = 0; y < height/50; y++){
+          g.fillOval(x*50, y*50, 30, 30);
+        }
       }
     }
+
   }
 
 
 }
 class ButtonPanel extends JPanel{
+
+  public JButton draw = new JButton("Draw");
+  public JButton exit = new JButton("Exit");
   public ButtonPanel(){
     this.setLayout(new FlowLayout());
-    this.add(new JButton("Draw"));
-    this.add(new JButton("Exit"));
+    this.add(draw);
+    this.add(exit);
   }
 
 }
