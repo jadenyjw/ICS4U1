0422b5c4efe34820b12cad9c79e00b16814c466b
Jaden Wang <jadenyjw@gmail.com>
2017-04-19 13:40:41 -0400

    Finish lines

--- WangJ_Layouts/Main.java
+++ WangJ_Layouts/Main.java
@@ -8,7 +8,6 @@ Assignment: Layout Assignments
 public class Main{
   public static void main(String[] args){
 
-
     Q2 q2 = new Q2();
 
     Q3 q3 = new Q3();
--- WangJ_PaintPanel/PaintInPanel.java
+++ WangJ_PaintPanel/PaintInPanel.java
@@ -9,11 +9,15 @@ Assignment: Paint in Panel
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 
 public class PaintInPanel extends JFrame{
 
   public PaintInPanel(){
+
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     double height = screenSize.getHeight();
 
@@ -31,51 +35,96 @@ public class PaintInPanel extends JFrame{
 
 class PaintPanel extends JPanel implements ActionListener{
 
-  CirclesPanel circles = new CirclesPanel();
+  private Timer timer;
+  LinesPanel lines = new LinesPanel();
   ButtonPanel buttons = new ButtonPanel();
   public PaintPanel(){
+    timer = new Timer(5,this);
+    timer.start();
     this.setLayout(new BorderLayout());
-    this.add(circles, BorderLayout.CENTER);
+    this.add(lines, BorderLayout.CENTER);
     buttons.draw.addActionListener(this);
     buttons.exit.addActionListener(this);
     this.add(buttons, BorderLayout.SOUTH);
+
   }
 
   public void actionPerformed(ActionEvent e){
-    if(e.getActionCommand().equals("Draw")){
-      circles.drawing=true;
-      circles.repaint();
+
+    if(e.getSource() == timer){
+      lines.repaint();
     }
-    else{
+
+    else if(e.getActionCommand().equals("Draw")){
+      lines.drawing=true;
+      lines.repaint();
+    }
+    else if(e.getActionCommand().equals("Exit")){
       System.exit(0);
     }
+
   }
 
 
 }
 
-class CirclesPanel extends JPanel{
+class LinesPanel extends JPanel{
 
   public boolean drawing = false;
+  ArrayList<Line> lines = new ArrayList<Line>();
+  public LinesPanel(){
+    int height = this.getHeight();
+    int width = this.getWidth();
+    for(int x = 0; x < 50; x++){
+      for(int y = 0; y < 50; y++){
+        lines.add(new Line(x*20, y*20));
 
-  public CirclesPanel(){
-
+      }
+    }
   }
 
   public void paintComponent(Graphics g){
     super.paintComponent(g);
-
     if(drawing){
-      int height = this.getHeight();
-      int width = this.getWidth();
-
-      for(int x = 0; x < width/50; x++){
-        for(int y = 0; y < height/50; y++){
-          g.fillOval(x*50, y*50, 30, 30);
-        }
+      for(int x = 0; x < lines.size(); x++){
+        lines.get(x).rotate();
+        lines.get(x).draw(g);
       }
     }
 
+  }
+
+
+}
+
+class Line{
+
+  int x1, x2, y1, y2, angle;
+  final int length = 20;
+
+  public Line(int x1, int y1){
+    Random random = new Random();
+    this.x1 = x1;
+    this.x2 = (int)(x1 + length*Math.sin(Math.toRadians(angle)));
+    this.y1 = y1;
+    this.y2 = (int) (y1 + length*Math.cos(Math.toRadians(angle)));
+    this.angle = random.nextInt(360);
+
+  }
+
+  void draw(Graphics g){
+    g.drawLine(x1, y1, x2, y2);
+  }
+
+  void rotate(){
+    if(angle == 360){
+      angle = 0;
+    }
+    else{
+      angle++;
+    }
+    this.x2 = (int)(x1 + length*Math.sin(Math.toRadians(angle)));
+    this.y2 = (int) (y1 + length*Math.cos(Math.toRadians(angle)));
   }
 
 
